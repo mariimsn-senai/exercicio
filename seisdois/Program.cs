f20336f13aad6f3b97f19aec8ef4d8040376f335
marina souza <marinax.s@hotmail.com>
2019-09-26 17:16:50 -0300

    29/09/2019

--- seisdois/Program.cs
+++ seisdois/Program.cs
@@ -5,27 +5,25 @@ namespace seisdois
     class Program
     {
         static void Main(string[] args)
-        {
-                int num;
-                int i;
-                 
-            Console.Write("digite primeiro numero:");  
-            num = int.Parse(Console.ReadLine());  
-
-     
-
-        { 
-           for(i = 0; i < num; i++){
-                num [i] = i;
-
-           }
-
-             if (num % 2 == 0)
-                Console.WriteLine("é par");
-            else 
-                Console.WriteLine("é impar");
+    {
+        int cont;
+        int par = 0;
+        int impar = 0;
+        int[] vetor = new int[6];
+        for ( cont = 0; cont < 6; cont++){ 
+        Console.Write($"digite o {cont+1} número");
+        vetor[cont] = int.Parse(Console.ReadLine());
+        }
+        foreach(int nem in vetor){
+            if (nem % 2 == 0){
+                    par++;
+            }else {
+                impar++;
+            }
+         
 
         }
+        Console.WriteLine($"Você tem {par} numeros pares e {impar} numeros impar"); 
     }
   }
 }
---
+++ temperatura/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace temperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] temperatura =new double[12];
+            double maior = 0;
+            double menor = 0;
+            int i;
+
+           for(i = 0; i < 12; i++)
+           {
+               Console.WriteLine($"digite a temperatura do mes:{i +1}:");
+               temperatura[i] = double.Parse(Console.ReadLine());
+           }
+           maior = temperatura [0];
+           menor = temperatura [0];
+
+           foreach(double temp in temperatura)
+           {
+               if(temp > maior)
+               {
+                   maior = temp;
+               } else if (temp < menor)
+               {
+                   menor = temp;
+               }
+               
+           }
+           Console.WriteLine($"A maior temperatura {maior} || a menor tempratura {menor}");
+        }
+    }
+}
